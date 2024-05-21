e4aa4000f437b2ef1488159c551806635616c4b3
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-21 21:44:03 +0200

    create note struct

---
+++ model/note.go
@@ -0,0 +1,10 @@
+package model
+
+import "gorm.io/gorm"
+
+type Note struct {
+	gorm.Model
+	Title   string `gorm:"size:40;not null" json:"title" validate:"required,gte=1,lte=40"`
+	Content string `gorm:"type:text" json:"content" validate:"required,lte=255"`
+	UserID  uint   `validate:"omitempty" json:"-"`
+}
