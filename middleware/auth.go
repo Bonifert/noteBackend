d4ef6efd2d342ba623f512fdfeac47eb97391f65
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-22 13:58:35 +0200

    create auth middleware

---
+++ middleware/auth.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"awesomeProject/config"
+	"awesomeProject/service"
+	"context"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"strings"
+)
+
+var secretKey = []byte(config.Config("SECRET_KEY"))
+
+func AuthenticateMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header required", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.ParseWithClaims(tokenString, &service.JwtCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return secretKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			fmt.Println(err)
+			return
+		}
+
+		claims, ok := token.Claims.(*service.JwtCustomClaims)
+		if !ok {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			fmt.Println(claims)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "id", claims.ID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
