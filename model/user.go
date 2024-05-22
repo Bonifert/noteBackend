4a0584738e87280d397c2c667981a02489c46ed4
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-22 13:56:27 +0200

    add tags

--- model/user.go
+++ model/user.go
@@ -7,10 +7,10 @@ import (
 
 type User struct {
 	gorm.Model
-	Username  string    `json:"username" validate:"required,gte=1,lte=255"`
-	Password  string    `json:"password" validate:"required,gte=1,lte=255"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	Username  string    `json:"username" validate:"required,gte=1,lte=255" gorm:"unique"`
+	Password  string    `json:"password" validate:"required,gte=1,lte=255" gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime,notnull" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime,notnull" json:"updated_at"`
 	Notes     []Note    `gorm:"foreignKey:UserID" json:"notes"`
 	DeletedAt gorm.DeletedAt
 }
