0672e2d0c82ddeb82d37249bc23df0d1fdc07a3a
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-23 23:15:31 +0200

    refactor dtos

--- model/note.go
+++ model/note.go
@@ -1,9 +1,7 @@
 package model
 
-import "gorm.io/gorm"
-
 type Note struct {
-	gorm.Model
+	GormModel
 	Title   string `gorm:"size:40;not null" json:"title" validate:"required,gte=1,lte=40"`
 	Content string `gorm:"type:text" json:"content" validate:"required,lte=255"`
 	UserID  uint   `validate:"omitempty" json:"-"`
--- model/user.go
+++ model/user.go
@@ -1,16 +1,8 @@
 package model
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
-
 type User struct {
-	gorm.Model
-	Username  string    `json:"username" validate:"required,gte=1,lte=255" gorm:"unique"`
-	Password  string    `json:"password" validate:"required,gte=1,lte=255" gorm:"not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime,notnull" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime,notnull" json:"updated_at"`
-	Notes     []Note    `gorm:"foreignKey:UserID" json:"notes"`
-	DeletedAt gorm.DeletedAt
+	GormModel
+	Username string `json:"username" validate:"required,gte=1,lte=255" gorm:"unique"`
+	Password string `json:"-" validate:"required,gte=1,lte=255" gorm:"not null"`
+	Notes    []Note `json:"-" gorm:"foreignKey:UserID"`
 }
