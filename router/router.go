9243aca643d64bbf94d3ee69cac9aed9ece777e6
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-30 13:32:33 +0200

    add routes

--- router/router.go
+++ router/router.go
@@ -20,10 +20,20 @@ func publicRoutes(router *http.ServeMux) {
 
 func protectedRoutes(mux *http.ServeMux) {
 	protectedRouter := http.NewServeMux()
+
+	// user related protected routes
 	protectedRouter.HandleFunc("GET /user/me", handler.GetMe)
 	protectedRouter.HandleFunc("GET /user/{id}", handler.GetUser)
+	protectedRouter.HandleFunc("DELETE /user/me", handler.DeleteMe)
+	protectedRouter.HandleFunc("PATCH /user/edit/username", handler.EditUsername)
+	protectedRouter.HandleFunc("PATCH /user/edit/password", handler.EditPassword)
+
+	// note related protected routes
 	protectedRouter.HandleFunc("POST /note", handler.CreateNote)
 	protectedRouter.HandleFunc("GET /note/{id}", handler.GetNote)
-	protectedRouter.HandleFunc("DELETE /user/me", handler.DeleteMe)
+	protectedRouter.HandleFunc("DELETE /note/{id}", handler.DeleteNote)
+	protectedRouter.HandleFunc("PATCH /note/content/{id}", handler.EditNoteContent)
+	protectedRouter.HandleFunc("PATCH /note/title/{id}", handler.EditNoteTitle)
+
 	mux.Handle("/", middleware.AuthenticateMiddleware(protectedRouter))
 }
