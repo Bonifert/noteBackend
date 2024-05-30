cc4b76b50fb5bd1a51fdefe9140288fe56616b67
Bonifert <116727997+Bonifert@users.noreply.github.com>
2024-05-30 13:23:20 +0200

    create getNoteById helper method to get the user by id, refactor the existing functions and create new functions for deleting and editing notes

--- service/note.go
+++ service/note.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"awesomeProject/database"
+	"awesomeProject/dto"
 	"awesomeProject/model"
 	"errors"
 )
@@ -22,11 +23,66 @@ func CreateNote(newNote *model.Note) (uint, error) {
 	return newNote.ID, nil
 }
 
-func GetNote(noteId uint) (*model.Note, error) {
-	var note model.Note
-	result := database.DB.Where("ID = ?", noteId).First(&note)
+func DeleteNote(nodeId uint, userId uint) error {
+	note, err := getNoteById(nodeId)
+	if err != nil {
+		return err
+	}
+	if note.UserID != userId {
+		return ErrForbidden
+	}
+	database.DB.Delete(&note)
+	return nil
+}
+
+func EditNoteContent(userId uint, noteId uint, editNoteContent *dto.EditNoteContent) error {
+	note, err := getNoteById(noteId)
+	if err != nil {
+		return err
+	}
+	if note.UserID != userId {
+		return ErrForbidden
+	}
+	note.Content = editNoteContent.NewContent
+	result := database.DB.Save(&note)
 	if result.Error != nil {
-		return nil, result.Error
+		return result.Error
+	}
+	return nil
+}
+
+func EditNoteTitle(userId uint, noteId uint, editNoteTitle *dto.EditNoteTitle) error {
+	note, err := getNoteById(noteId)
+	if err != nil {
+		return err
+	}
+	if note.UserID != userId {
+		return ErrForbidden
+	}
+	note.Title = editNoteTitle.NewTitle
+	result := database.DB.Save(note)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func GetNote(noteId uint, userId uint) (*model.Note, error) {
+	note, err := getNoteById(noteId)
+	if err != nil {
+		return nil, err
+	}
+	if note.UserID != userId {
+		return nil, ErrForbidden
+	}
+	return note, nil
+}
+
+func getNoteById(noteId uint) (*model.Note, error) {
+	var note model.Note
+	result := database.DB.Where("id = ?", noteId).First(&note)
+	if result.Error != nil {
+		return nil, ErrNotFound
 	}
 	return &note, nil
 }
